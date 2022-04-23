ef25b532665a36c2845a0eb69a00b2b32b557691
Payal <98936150+GargPayal@users.noreply.github.com>
2022-04-23 12:11:52 +0530

    second commit

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
 const button = document.querySelector('.btn btn-primary about');
 button.addEventListener('click',()=>{
-    window.open(about.html);
+    window.open(omtech-website/about.html);
 
 })
 const centre = document.querySelector('.centre');
 centre.addEventListener('click',()=>{
-    window.open(construction.html);
+    window.open(omtech-website/construction.html);
 }
 )
 const artitech = document.querySelector('.artitech');
 artitech.addEventListener('click',()=>{
-    window.open(artitech.html);
+    window.open(omtech-website/artitech.html);
 }
 )
 const drone = document.querySelector('.drone');
 drone.addEventListener('click',()=>{
-    window.open(drone.html);
+    window.open(omtech-website/drone.html);
 }
 )
